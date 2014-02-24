f9ce75009ee612aa311b0b724f0abc838c9dd53b
Alan Yeo Jie Wei <yeojw10@gmail.com>
2014-02-25 02:13:35 +0800

    Prompt for date period before listing time shifts

--- cli.rb
+++ cli.rb
@@ -3,6 +3,7 @@ require 'highline/import'
 require 'terminal-table'
 
 ROOT_URL = "http://pivotaltracker.com/"
+DATE_FORMAT = "%m/%d/%Y"
 
 agent = Mechanize.new
 login_page = agent.get ROOT_URL + 'signin'
@@ -16,8 +17,33 @@ form["credentials[username]"] = username
 form["credentials[password]"] = password
 form.submit
 
+def get_current_period
+  today = Date.today
+  day = today.day
+  month = today.month
+  year = today.year
+
+  if day > 15
+    first_day, last_day = 16, -1
+  else
+    first_day, last_day = 1, 15
+  end
+
+  [ Date.civil(year, month, first_day).strftime(DATE_FORMAT),
+    Date.civil(year, month, last_day).strftime(DATE_FORMAT) ]
+end
+
 # time shifts page
 time_shifts_page = agent.get ROOT_URL + 'time_shifts'
+user_id = time_shifts_page.parser.css('#shift_person_id option[selected="selected"]').attr('value').text() 
+
+default_start_date, default_end_date = get_current_period
+
+start_date = ask("Start Date (MM/DD/YYYY): ") { |q| q.default = default_start_date }
+end_date = ask("End Date (MM/DD/YYYY): ") { |q| q.default = default_end_date }
+
+url_with_date_range = URI::escape(ROOT_URL + "time_shifts?date_period[start]=#{start_date}&date_period[finish]=#{end_date}&project=-1&person=#{user_id}&location=none&grouped_by=&commit=Submit")
+time_shifts_page = agent.get url_with_date_range
 time_shift_rows = time_shifts_page.parser.css('#shift_table tr')
 
 table = Terminal::Table.new do |t|

f583d511d36dcc1aa9854f3a361d3881e0135602
Alan Yeo Jie Wei <yeojw10@gmail.com>
2014-02-26 01:00:28 +0800

    Refactor into TimeShift class

--- cli.rb
+++ cli.rb
@@ -5,17 +5,54 @@ require 'terminal-table'
 ROOT_URL = "http://pivotaltracker.com/"
 DATE_FORMAT = "%m/%d/%Y"
 
-agent = Mechanize.new
-login_page = agent.get ROOT_URL + 'signin'
+class TimeShift
+  def initialize username, password
+    @agent = Mechanize.new
+    login_for username, password
+  end
 
-# ask for login credentials
-username = ask("Username: ")
-password = ask("Password: ") { |q| q.echo = false }
+  def print_time_shifts_for start_date, end_date
+    time_shifts_page = @agent.get ROOT_URL + 'time_shifts'
+    user_id = time_shifts_page.parser.css('#shift_person_id option[selected="selected"]').attr('value').text()
 
-form = login_page.forms[0]
-form["credentials[username]"] = username
-form["credentials[password]"] = password
-form.submit
+    url_with_date_range = URI::escape(ROOT_URL + "time_shifts?date_period[start]=#{start_date}&date_period[finish]=#{end_date}&project=-1&person=#{user_id}&location=none&grouped_by=&commit=Submit")
+    time_shifts_page = @agent.get url_with_date_range
+    time_shift_rows = time_shifts_page.parser.css('#shift_table tr')
+
+    table = Terminal::Table.new do |t|
+      prev_entry = ""
+      index = 0
+      noOfRows = time_shift_rows.length - 2 # ignore table header and footer
+
+      time_shift_rows[1..-2].each do |row|
+        day     = row.css('td.date')[0].text().strip()
+        date    = row.css('td.date')[1].text().strip()
+        project = row.css('td.project').text().strip()
+        hours   = row.css('td.hours').text().strip()
+
+        if prev_entry != day and index.between?(1, noOfRows - 1)
+          t.add_separator
+        end
+        t.add_row [day, date, project, hours]
+
+        index += 1
+        prev_entry = day
+      end
+    end
+
+    puts table
+  end
+
+  private
+  def login_for username, password
+    login_page = @agent.get ROOT_URL + 'signin'
+
+    form = login_page.forms[0]
+    form["credentials[username]"] = username
+    form["credentials[password]"] = password
+    form.submit
+  end
+end
 
 def get_current_period
   today = Date.today
@@ -33,40 +70,16 @@ def get_current_period
     Date.civil(year, month, last_day).strftime(DATE_FORMAT) ]
 end
 
-# time shifts page
-time_shifts_page = agent.get ROOT_URL + 'time_shifts'
-user_id = time_shifts_page.parser.css('#shift_person_id option[selected="selected"]').attr('value').text() 
+username = ask("Username: ")
+password = ask("Password: ") { |q| q.echo = false }
+
+time_shift = TimeShift.new username, password
 
 default_start_date, default_end_date = get_current_period
 
 start_date = ask("Start Date (MM/DD/YYYY): ") { |q| q.default = default_start_date }
 end_date = ask("End Date (MM/DD/YYYY): ") { |q| q.default = default_end_date }
 
-url_with_date_range = URI::escape(ROOT_URL + "time_shifts?date_period[start]=#{start_date}&date_period[finish]=#{end_date}&project=-1&person=#{user_id}&location=none&grouped_by=&commit=Submit")
-time_shifts_page = agent.get url_with_date_range
-time_shift_rows = time_shifts_page.parser.css('#shift_table tr')
-
-table = Terminal::Table.new do |t|
-  prev_entry = ""
-  index = 0
-  noOfRows = time_shift_rows.length - 2 # ignore table header and footer
-
-  time_shift_rows[1..-2].each do |row|
-    day     = row.css('td.date')[0].text().strip()
-    date    = row.css('td.date')[1].text().strip()
-    project = row.css('td.project').text().strip()
-    hours   = row.css('td.hours').text().strip()
-
-    if prev_entry != day and index.between?(1, noOfRows - 1)
-      t.add_separator
-    end
-    t.add_row [day, date, project, hours]
-
-    index += 1
-    prev_entry = day
-  end
-end
-
-puts table
+time_shift.print_time_shifts_for start_date, end_date
 
 #create_new_shifts_page = agent.get ROOT_URL + 'time_shifts/new'
